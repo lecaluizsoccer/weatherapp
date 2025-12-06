4a9e217d1c18916668dc3a93d9f1177631687809
lecaluizsoccer <lecaluizsoccer@gmail.com>
2025-12-06 22:18:07 +0100

    new improvements

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-let weatherForcast = {}
+let weatherForecast = {}
 const place = document.querySelector("#input-text");
 const body = document.querySelector("body");
-let unit = "metric"
+const local = document.querySelector(".local");
+const temp = document.querySelector(".temp");
+const feelsLike = document.querySelector(".feels-like");
+const maxTemp = document.querySelector(".max-temp");
+const minTemp = document.querySelector(".min-temp");
+const humidity = document.querySelector(".humidity");
+const windSpeed = document.querySelector(".wind-speed");
+const visibility = document.querySelector(".visibility");
 
-let heat = "°C";
-let speed = "kph"
-let dist = "km"
+
+const weatherMetric = {
+  unit: "metric",
+  heat: "°C",
+  speed: "kph",
+  dist: "km"
+};
+
+const weatherImperial = {
+  unit: "imperial",
+  heat: "°F",
+  speed: "mph",
+  dist: "mi",
+};
+
+let unit = weatherMetric.unit;
+let heat = weatherMetric.heat;
+let speed = weatherMetric.speed;
+let dist = weatherMetric.dist;
+
+
 
 function isInputFilled() {
   return place.value.trim() !== "";
@@ -18,23 +43,19 @@ place.addEventListener("keydown", (e) => {
 });
 
 body.addEventListener("click", (e) => {
-  if(isInputFilled()){
     if (e.target.classList.contains("celsius")) {
-      unit = "metric";
-      heat = "°C";
-      speed = "kph";
-      dist = "km";
+      unit = weatherMetric.unit;
+      heat = weatherMetric.heat;
+      speed = weatherMetric.speed;
+      dist = weatherMetric.dist;
       getWeather();
     } else if (e.target.classList.contains("fahrenheit")) {
-      unit = "imperial";
-      heat = "°F";
-      speed = "mph";
-      dist = "mi";
+      unit = weatherImperial.unit;
+      heat = weatherImperial.heat;
+      speed = weatherImperial.speed;
+      dist = weatherImperial.dist;
       getWeather();
     }
-  } else {
-    document.querySelector(".local").textContent = "Type a location"
-  }
 })
 
 
@@ -45,26 +66,37 @@ function getWeather(){
       fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?q=${place.value}&units=${unit}`)
       .then((response) => response.json())
       .then((data) => {
-          Object.assign(weatherForcast, data);
-          console.log(weatherForcast);
+          Object.assign(weatherForecast, data);
+          console.log(weatherForecast);
           updateWeatherDisplay()
       });
-  } else {
-    document.querySelector(".local").textContent = "Type a location";
-  }
-    
+    } else {
+            fetch(
+              `https://apis.scrimba.com/openweathermap/data/2.5/weather?q=vila barros&units=${unit}`
+            )
+              .then((response) => response.json())
+              .then((data) => {
+                Object.assign(weatherForecast, data);
+                console.log(weatherForecast);
+                updateWeatherDisplay();
+              });
+      
+    }
+      
 }
 
 function updateWeatherDisplay() {
-    document.querySelector(".local").textContent = `⚐ ${weatherForcast.name}`;
-    document.querySelector(".temp").textContent = `${Math.round(weatherForcast.main.temp)}${heat}`;
-    document.querySelector(".feels-like").textContent = `${Math.round(weatherForcast.main.feels_like)}${heat}`;
-    document.querySelector(".max-temp").textContent = `Max ${Math.round(weatherForcast.main.temp_max)}${heat}`;
-    document.querySelector(".min-temp").textContent = `Min ${Math.round(weatherForcast.main.temp_min)}${heat}`;
+    local.textContent = `⚐ ${weatherForecast.name}`;
+    temp.textContent = `${Math.round(weatherForecast.main.temp)}${heat}`;
+    feelsLike.textContent = `${Math.round(weatherForecast.main.feels_like)}${heat}`;
+    maxTemp.textContent = `Max ${Math.round(weatherForecast.main.temp_max)}${heat}`;
+    minTemp.textContent = `Min ${Math.round(weatherForecast.main.temp_min)}${heat}`;
 
-    document.querySelector(".humidity").textContent = `${Math.round(weatherForcast.main.humidity)}%`;
-    document.querySelector(".wind-speed").textContent = `${Math.round(weatherForcast.wind.speed)} ${speed}`;
-    document.querySelector(".visibility").textContent = `${Math.round(weatherForcast.visibility / 1000)} ${dist}`;
+    humidity.textContent = `${Math.round(weatherForecast.main.humidity)}%`;
+    windSpeed.textContent = `${Math.round(weatherForecast.wind.speed)} ${speed}`;
+    visibility.textContent = `${Math.round(weatherForecast.visibility / 1000)} ${dist}`;
 
 }
+
+getWeather();
 
